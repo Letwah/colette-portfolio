aef44b733f11d305d5f02eb9bd3c141f890867da
Colette <hello@colettesmith.com>
2023-09-07 19:20:11 +0100

    emotion comp test

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 // import { motion } from "framer-motion";
 import "./projects/projects.css";
+import HoverVideoPlayer from "react-hover-video-player";
+import PausedOverlay from "./projects/PausedOverlay";
+import LoadingOverlay from "./projects/LoadingOverlay";
 
 const Projects = () => {
   return (
     <>
       <section className="projects" id="projects">
         <div className="projectsContainer top">
+          <HoverVideoPlayer
+            videoSrc="https://youtu.be/C3p7ACl4qEM"
+            pausedOverlay={<PausedOverlay />}
+            loadingOverlay={<LoadingOverlay />}
+          />
           <div className="projectsCard">
             <div className="projectImg">
               <img src="./assets/images/hero/Mockup.jpg"></img>
---
+++ src/components/projects/PausedOverlay.jsx
@@ -0,0 +1,43 @@
+import { css } from "@emotion/css";
+
+const PausedOverlay = () => (
+  <div>
+    <img
+      src="/assets/images/projects/VanillaSlideThumb.png"
+      alt=""
+      className={css`
+        /* Thumbnail image expands to cover the player */
+        position: absolute;
+        width: 100%;
+        height: auto;
+        top: 0;
+        left: 0;
+        object-fit: cover;
+      `}
+    />
+    <div
+      className={css`
+        /* Ensure the description text is displayed on top of the thumbnail image */
+        z-index: 1;
+        font-family: sans-serif;
+        /* Position the text in the bottom-left corner of the overlay */
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        padding: 1em;
+
+        h1 {
+          margin: 0 0 0.2em;
+        }
+        p {
+          margin: 0 0.2em 0;
+        }
+      `}
+    >
+      <h1>Vanilla Slide Show</h1>
+      <p>JavaScript Image Search, hover to play video preview</p>
+    </div>
+  </div>
+);
+
+export default PausedOverlay;
