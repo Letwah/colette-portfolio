4f8851ba46500c7171ccfc6585e2f3b2d491d2c1
Colette <hello@colettesmith.com>
2024-02-17 13:42:26 +0000

    featured image in slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Slides } from "./slider/Slides";
 
 import "./slider/slider.css";
--- src/components/slider/image-data.ts
+++ src/components/slider/image-data.ts
@@ -1,4 +1,5 @@
 export const images = [
+  "./assets/images/hero/slide-0.jpg",
   "./assets/images/hero/slide-1.jpg",
   "./assets/images/hero/slide-2.jpg",
   "./assets/images/hero/slide-3.jpg",
